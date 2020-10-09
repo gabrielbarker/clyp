58b1279f95a88c4cc7729721330997c4ca3fb068
Gabriel Barker <gabrielbarker@gmail.com>
2020-10-09 16:00:41 +0100

    IMPLEMENT reading and writing to clipboard

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,44 @@
-use anyhow::{Context, Result};
+use anyhow::Result;
+use clipboard::{ClipboardContext, ClipboardProvider};
 use structopt::StructOpt;
 
 /// Search for a pattern in a file and display the lines that contain it.
 #[derive(StructOpt)]
 struct Cli {
-    /// The pattern to look for
-    pattern: String,
-    /// The path to the file to read
-    #[structopt(parse(from_os_str))]
-    path: std::path::PathBuf,
+    /// Activate copy mode
+    #[structopt(short, long)]
+    copy: bool,
+    /// Activate paste mode
+    #[structopt(short, long)]
+    paste: bool,
+    /// The name of the clyp
+    #[structopt()]
+    name: String,
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let path = &Cli::from_args().path;
-    let content = std::fs::read_to_string(path)
-        .with_context(|| format!("could not read file `{}`", path.display()))?;
-    println!("file content: {}", content);
+    let clyps_folder = "/Users/gbarker/GitHub/clyp/clyps";
+    let path: std::path::PathBuf = [clyps_folder, &Cli::from_args().name].iter().collect();
+    let mut ctx: ClipboardContext = ClipboardProvider::new().unwrap();
+    if Cli::from_args().copy {
+        let content = ctx.get_contents().expect("could not read from clipboard");
+        save_to_file(path, content);
+    } else if Cli::from_args().paste {
+        let content = read_from_file(path);
+        ctx.set_contents(content)
+            .expect("could not add to clipboard");
+    }
     Ok(())
+}
+
+fn save_to_file(path: std::path::PathBuf, content: String) {
+    std::fs::write(path, content).expect("Unable to write file");
+}
+
+fn read_from_file(path: std::path::PathBuf) -> String {
+    let content =
+        std::fs::read_to_string(&path).expect(&format!("could not read file `{}`", path.display()));
+    return content;
 }
 
 #[cfg(test)]
