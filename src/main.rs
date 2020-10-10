83a8415703aaa8a0c687d813c2635fba6d04b786
Gabriel Barker <gabrielbarker@gmail.com>
2020-10-10 01:54:30 +0100

    REFACTOR the app

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,8 @@
-use anyhow::Result;
-use clipboard::{ClipboardContext, ClipboardProvider};
-use structopt::StructOpt;
-
 mod clyprepo;
-use clyprepo::ClypRepository;
+mod command;
 
-/// Save and read clyps.
-#[derive(StructOpt)]
-struct Opts {
-    /// Activate save mode
-    #[structopt(short, long)]
-    save: bool,
-    /// Activate read mode
-    #[structopt(short, long)]
-    read: bool,
-    /// Clear all saved clyps
-    #[structopt(short, long)]
-    clear: bool,
-    /// The name of the clyp
-    #[structopt(required_unless_one(&["clear", "help"]))]
-    name: Option<String>,
-}
+use command::run_clyp_command;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let repo = ClypRepository::new("/Users/gbarker/GitHub/clyp/clyps".to_string());
-    let opts = Opts::from_args();
-
-    if opts.clear {
-        repo.clear_clyps();
-    } else {
-        save_or_read_clyp(opts.save, opts.read, opts.name.unwrap(), repo);
-    }
-    Ok(())
-}
-
-fn save_or_read_clyp(save: bool, read: bool, name: String, repo: ClypRepository) {
-    let mut ctx: ClipboardContext = ClipboardProvider::new().unwrap();
-
-    if save {
-        let content = ctx.get_contents().expect("could not read from clipboard");
-        repo.save_clyp(name, content);
-    } else if read {
-        let content = repo.read_clyp(name);
-        ctx.set_contents(content)
-            .expect("could not add to clipboard");
-    }
+    return run_clyp_command("/Users/gbarker/GitHub/clyp/clyps");
 }
